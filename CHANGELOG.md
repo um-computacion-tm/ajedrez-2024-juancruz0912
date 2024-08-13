086ccb5734ecee70b074698703518686189bda7b
juancruz0912 <j.rupcic@alumno.um.edu.ar>
2024-08-13 12:31:07 -0300

    actualizar archivo main

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,8 +1,16 @@
 # CHANGELOG
 
+## [1.0.1] - 13-08-2024
+
+### Agregados
+- Archivo test.py, el cual tiene los test del archivo main
+
+## Modificado
+- Archivo main.py: se creo la funcion principal con la cual se va a iniciar el juego
+
 ## [1.0.0] - 06-08-2024.
 
-### Agregar
+### Agregados
 
 - Archivo README
 - Archivo CHANGELOG
--- main.py
+++ main.py
@@ -1,2 +1,18 @@
-def ajedrez():
-    return "Juego del Ajedrez"
+from Clases.Juego import Juego
+
+def main():
+    
+    print('Bienvenidos al juego del ajedrez')
+    print('Ingresar los nombres de los jugadores')
+    
+    jugador1 = input('Jugador 1: ')
+    jugador2 = input('Jugador 2: ')
+    juego = Juego('tablero', jugador1, jugador2)
+    juego.empezar_juego()
+    
+    while juego.__estado__ == 1:
+        estado = int(input('Ingresar 1 para seguir jugando o 0 para terminar el juego'))
+        if estado == 0:
+            juego.terminar_juego()
+
+main()
