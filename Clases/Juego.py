c62ed4ec440f3c9b73f0c67b895866778509b822
juancruz0912 <j.rupcic@alumno.um.edu.ar>
2024-08-14 14:38:58 -0300

    Agregar clase Tablero

--- Clases/Juego.py
+++ Clases/Juego.py
@@ -1,16 +1,18 @@
+from Tablero import Tablero
+
 class Juego:
-    def __init__(self, tablero, jugador1, jugador2):
+    def __init__(self, jugador1, jugador2):
         self.__estado__ = 0
-        self.__tablero__ = tablero
+        self.__tablero__ = Tablero()
         self.__blanco__ = jugador1
         self.__negro__ = jugador2
         self.__turno__ = jugador1
-
-
+    
     #Metodo que permite iniciar el juego, estableciendo el atributo estado en 1 (Jugando)
     def empezar_juego(self):
         self.__estado__ = 1
         print('Empezo el juego')
+        self.__tablero__.imprimir_tablero()
 
     #Metodo que permite finalizar el juego, estableciendo el atributo estado en 0 (No Jugando) 
     def terminar_juego(self):
--- Clases/TestJuego.py
+++ Clases/TestJuego.py
@@ -3,23 +3,19 @@ from Juego import Juego
 
 class TestJuego(unittest.TestCase):
 
-    juego = Juego('tablero', 'Alice', 'Bob')
+    juego = Juego('Alice', 'Bob')
 
-    def test_initialization(self):
-        self.juego = Juego('tablero', 'Alice', 'Bob')
-        self.assertEqual(self.juego.__estado__, 0)
-        self.assertEqual(self.juego.__tablero__, 'tablero')
+    def test_iniciar(self):
+        self.assertEqual(self.juego.__estado__, 1)
         self.assertEqual(self.juego.__blanco__, 'Alice')
         self.assertEqual(self.juego.__negro__, 'Bob')
         self.assertEqual(self.juego.__turno__, 'Alice')
 
     def test_empezar_juego(self):
-        self.juego = Juego('tablero', 'Alice', 'Bob')
         self.juego.empezar_juego()
         self.assertEqual(self.juego.__estado__, 1)
 
     def test_terminar_juego(self):
-        self.juego = Juego('tablero', 'Alice', 'Bob')
         self.juego.terminar_juego()
         self.assertEqual(self.juego.__estado__, 0)
 
