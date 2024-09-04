bf80cab6ddfee2957ee08b5ab9276302285d49f0
juancruz0912 <j.rupcic@alumno.um.edu.ar>
2024-09-03 21:29:04 -0300

    Corrigiendo repeticiones

--- Juego/Clases/Piezas/Peon.py
+++ Juego/Clases/Piezas/Peon.py
@@ -6,37 +6,29 @@ class Peon(PiezaId):
     pieza_negra = '♟'
 
     def __init__(self, color, id, fila, columna, movimiento = None):
+        self.__primer_movimiento__ = False
         super().__init__('Peon', color, id, fila, columna, movimiento)
 
     def verificar_movimiento(self, fila, columna):
         if self.columna == columna:
-            paso = -1 if self.__color__ == 'blanco' else 1
-            if self.primer_movimiento(fila):
-                return True
+            paso = -1 if self.color == 'blanco' else 1
+            if self.__primer_movimiento__ == False:
+                self.primer_movimiento(fila, paso)
             elif fila == self.fila + paso:
                 self.__movimiento__ = 'Recto' 
                 return True
             else:
                 raise ValueError('El peon se puede mover una casilla hacia adelante')
-        elif abs(self.__fila__ - fila) == 1 and abs(self.__columna__ - columna) == 1:
+        elif abs(self.fila - fila) == 1 and abs(self.columna - columna) == 1:
             self.__movimiento__ = 'Comer' 
             return True
         else:
             raise ValueError('El peon solo se puede mover en línea recta')
         
-    def primer_movimiento(self, fila):
-        if self.__color__ == 'blanco':
-            if self.fila == 7:
-                if fila == 6 or fila == 5:
-                    self.__movimiento__ = 'Recto' 
-                    return True
-                else:
-                    raise ValueError('El peon se puede mover una o dos casillas hacia adelante')
-        elif self.__color__ == 'negro':
-            if self.fila == 2:
-                if fila == 3 or fila == 4:
-                    self.__movimiento__ = 'Recto' 
-                    return True
-                else:
-                    raise ValueError('El peon se puede mover una o dos casillas hacia adelante')
-        
+    def primer_movimiento(self, fila, paso):
+        if fila == self.fila + paso or fila == self.fila + 2 * paso:
+            self.__movimiento__ = 'Recto'
+            self.__primer_movimiento__ = True
+            return True
+        else:
+            raise ValueError('El peon se puede mover una o dos casillas hacia adelante')
--- Juego/Tests/test_reina.py
+++ Juego/Tests/test_reina.py
@@ -14,17 +14,11 @@ class TestReina(unittest.TestCase):
         self.assertEqual(self.reina_negra.__color__, 'negro')
 
     def test_movimiento_recto_valido(self):
-        movimiento_b = self.reina_blanca.verificar_movimiento(8, 7)
-        self.assertEqual(movimiento_b, True)
-        self.assertEqual(self.reina_blanca.movimiento, 'Recto')
-        
-        movimiento_b2 = self.reina_blanca.verificar_movimiento(8, 7)
-        self.assertEqual(movimiento_b2, True)
+        self.reina_blanca.verificar_movimiento(8, 7)
         self.assertEqual(self.reina_blanca.movimiento, 'Recto')
 
     def test_movimiento_diagonal_valido(self):
-        movimiento_b = self.reina_blanca.verificar_movimiento(7, 3)
-        self.assertEqual(movimiento_b, True)
+        self.reina_blanca.verificar_movimiento(7, 3)
         self.assertEqual(self.reina_blanca.movimiento, 'Diagonal')
 
     def test_movimiento_invalido(self):
