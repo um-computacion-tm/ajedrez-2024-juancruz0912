c9b80280083487436a74f38d73d563cbebdddf74
juancruz0912 <j.rupcic@alumno.um.edu.ar>
2024-09-17 16:01:55 -0300

    Eliminando repeticiones

--- Juego/Clases/Piezas/Peon.py
+++ Juego/Clases/Piezas/Peon.py
@@ -12,16 +12,9 @@ class Peon(PiezaPeon):
 
 
     def movimiento_especifico(self, fila, columna):
-        if self.columna == columna:
-            paso = -1 if self.color == 'blanco' else 1
-            if self.__primer_movimiento__ == False:
-                return self.primer_movimiento(fila, paso)
-            elif fila == self.fila + paso:
-                self.__movimiento__ = 'Recto' 
-                return True
-            else:
-                return False
-        elif self.un_paso(fila, columna):
+        if self.columna == columna: # caso en el que el peon se mueve recto
+            return self.movimiento_normal(fila)
+        elif self.un_paso(fila, columna): # caso donde el peon quiere comer una pieza
             return self.es_movimiento_diagonal(fila, columna, 'Comer')
         else:
             return False
@@ -30,6 +23,16 @@ class Peon(PiezaPeon):
         if fila == self.fila + paso or fila == self.fila + 2 * paso:
             self.__movimiento__ = 'Recto'
             self.__primer_movimiento__ = True
+            return True
+        else:
+            return False
+        
+    def movimiento_normal(self, fila):
+        paso = -1 if self.color == 'blanco' else 1
+        if self.__primer_movimiento__ == False:
+            return self.primer_movimiento(fila, paso)
+        elif fila == self.fila + paso:
+            self.__movimiento__ = 'Recto' 
             return True
         else:
             return False
