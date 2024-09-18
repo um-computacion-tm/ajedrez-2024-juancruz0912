440a6e20b406c499956cb417d89b542c6493ab55
juancruz0912 <j.rupcic@alumno.um.edu.ar>
2024-09-18 12:21:39 -0300

    Corrigiendo atributos

--- Juego/Piezas/caballo.py
+++ Juego/Piezas/caballo.py
@@ -7,13 +7,13 @@ class Caballo(PiezaId):
     c1 = 2  
     c2 = 7
 
-    def __init__(self, color, **kwargs):        
+    def __init__(self, color, **kwargs):         
         super().__init__('Caballo', color, id=kwargs['id'])
+        self.__movimiento__ = 'Caballo'
 
 
     def movimiento_especifico(self, fila, columna):
         if (abs(self.fila - fila) == 2 and abs(self.columna - columna) == 1) or (abs(self.fila - fila) == 1 and abs(self.columna - columna) == 2):
-            self.__movimiento__ = 'Caballo' 
             return True
         else:
             return False
